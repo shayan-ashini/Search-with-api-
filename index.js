afc05cc950ead6ceb8c6871d36b13cb92c100e7f
shayan <109408144+shayan-ashini@users.noreply.github.com>
2023-07-09 19:49:24 +0330

    Update index.js

--- index.js
+++ index.js
@@ -78,46 +78,6 @@ load()
 
 
 
-// const charactersList = document.getElementById('charactersList');
-// const searchBar = document.getElementById('searchBar');
-// let hpCharacters = [];
-
-// searchBar.addEventListener('keyup', (e) => {
-//     const searchString = e.target.value.toLowerCase();
-
-//     const filteredCharacters = hpCharacters.filter((character) => {
-//         return (
-//             character.name.toLowerCase().includes(searchString) ||
-//             character.house.toLowerCase().includes(searchString)
-//         );
-//     });
-//     displayCharacters(filteredCharacters);
-// });
-
-// const loadCharacters = async () => {
-    
-//         const res = await fetch('https://hp-api.herokuapp.com/api/characters');
-//         hpCharacters = await res.json();
-//         displayCharacters(hpCharacters);
-
-// };
-
-// const displayCharacters = (characters) => {
-//     const htmlString = characters
-//         .map((character) => {
-//             return `
-//             <li class="character">
-//                 <h2>${character.name}</h2>
-//                 <p>House: ${character.house}</p>
-//                 <img src="${character.image}"></img>
-//             </li>
-//         `;
-//         })
-//         .join('');
-//     charactersList.innerHTML = htmlString;
-// };
-
-// loadCharacters();
 
 
 
